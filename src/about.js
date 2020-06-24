d6c8f4e63df1f47d55bba2738d01af69029e0caf
Azrielle <azriellepelayo@gmail.com>
2020-06-25 00:55:54 +0800

    Created about.js and menu.js and imported them in index.js

---
+++ src/about.js
@@ -0,0 +1,37 @@
+let aboutPage = (() =>{
+   
+    let content = document.querySelector("#content");
+
+    const createAboutPage = () => {
+
+        let aboutPage = document.createElement("div");
+        aboutPage.className = "aboutPage";
+        aboutPage.setAttribute("class", "aboutPage");
+
+        let aboutTitle = document.createElement("h1");
+        aboutTitle.textContent = "ABOUT";
+   
+        let loremTitle = document.createElement("h2");
+        loremTitle.textContent = "Lorem Ipsum";
+
+        let firstPara = document.createElement("p");
+        firstPara.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id elit dolor. Praesent neque augue, lacinia in ipsum vel, ultricies tristique ex. Proin vulputate porta ligula, nec egestas nisi rhoncus non. Integer cursus lorem a libero cursus fringilla.";
+
+        let secondPara = document.createElement("p");
+        secondPara.textContent = "Phasellus mattis tortor tincidunt lacus fringilla, a euismod ante facilisis. Aliquam aliquet ornare enim, at lobortis sem porta ac. Pellentesque dignissim nec nunc sed lacinia.";
+
+        aboutPage.append(aboutTitle, loremTitle, firstPara, secondPara); 
+        
+        content.appendChild(aboutPage);
+    }
+
+    const createFooter = () =>{
+        let footer = document.createElement("footer");
+        footer.textContent = "SuShee  |  All Rights Reserved 2020";
+
+        content.appendChild(footer);
+    }
+    return {createAboutPage, createFooter};
+})();
+
+export default aboutPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import  homePage  from './home.js';
-
+import menu from './menu.js';
+import aboutPage from './about.js'; 
 
 
 homePage.createHomePage();
+menu.createMenuPage();
+aboutPage.createAboutPage();
+aboutPage.createFooter();
 
 
 
